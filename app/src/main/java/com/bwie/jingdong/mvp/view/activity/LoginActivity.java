e8653f54b0089f939eca801b4f35caf21ec6e40d
ZhouRuiQing <379996854@qq.com>
2018-11-30 12:04:08 +0800

    在LoginActivity添加了一行log

--- app/src/main/java/com/bwie/jingdong/mvp/view/activity/LoginActivity.java
+++ app/src/main/java/com/bwie/jingdong/mvp/view/activity/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -38,6 +39,7 @@ public class LoginActivity extends AppCompatActivity implements LoginView {
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
 
+        Log.i("aaa", "onCreate: =="+user);
         presentLogin = new PresentLogin(this);
 
     }
